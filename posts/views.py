86b3475337ca34ad46b12e649e1011f1878f96b6
HollyT85 <h.e.turner@gmail.com>
2023-09-15 18:00:31 +0000

    added templates dor posts and post-info

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Post
+
+def all_posts(request):
+    # View all rocks on the rock page
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'posts/posts.html', context)
+
+
+def post_info(request, post_id):
+    # Get individual rock/fossil info
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'posts/post-info.html', context)
+
+
+
+# @login_required
+# def add_post(request):
+#     if not request.user.is_superuser:
+#         messages.error(request, 'You are not authorised to do this.')
+#         return redirect(reverse('home'))
+
+    
--- rocks/admin.py
+++ rocks/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Rock
 
-# class rockAdmin(admin.ModelAdmin):
-#     list_display=['image']
-
 admin.site.register(Rock)
