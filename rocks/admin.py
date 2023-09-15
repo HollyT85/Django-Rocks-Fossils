b594e744eaa371a0576df4c74150507e759bf79c
HollyT85 <h.e.turner@gmail.com>
2023-09-15 16:25:34 +0000

    added code to make images work

--- rocks/admin.py
+++ rocks/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Rock
 
+# class rockAdmin(admin.ModelAdmin):
+#     list_display=['image']
+
 admin.site.register(Rock)
--- rocks_fossils/urls.py
+++ rocks_fossils/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('rocks/', include('rocks.urls')),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
