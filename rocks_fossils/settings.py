c8090322820dde2cd29901ac4378ecb454ea0152
HollyT85 <h.e.turner@gmail.com>
2023-09-28 17:52:34 +0000

    addded code to home page but not functioning fully yet so commented out

--- rocks_fossils/settings.py
+++ rocks_fossils/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-hollyt85-djangorocksfos-jsv5q6q67sw.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-hollyt85-djangorocksfos-jsv5q6q67sw.ws-eu105.gitpod.io']
 
 
 # Application definition
